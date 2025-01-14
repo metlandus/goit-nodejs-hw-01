89f4514bd63e1e978ad3fc7dd5496b2e888a526a
Metlandus <metlandus@hotmail.com.tr>
2025-01-14 18:52:23 +0300

    Homework is finished

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,18 @@
-export const removeLastContact = async () => {};
+import { writeContacts } from "../utils/writeContacts.js";
+import { countContacts } from "./countContacts.js";
+import { getAllContacts } from "./getAllContacts.js";
+
+export const removeLastContact = async () => {
+    let num = await countContacts();
+    if (num > 0) {
+        let contacts = JSON.parse(await getAllContacts());
+        const newContacts = contacts.slice(0, num - 1)
+        writeContacts(newContacts)
+        console.log(`Last Contact ${contacts[num - 1].name} Removed`)
+    }
+    else {
+        console.log("There are no contacts in the database")
+    }
+};
 
 removeLastContact();
