cdebbad57a3a304782fc11a8181b86f675956bae
Metlandus <metlandus@hotmail.com.tr>
2025-01-14 14:53:59 +0300

    Tasks 6 and 7 are done

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,3 +1,13 @@
-export const countContacts = async () => {};
+import { readContacts } from "../utils/readContacts.js";
+
+export const countContacts = async () => {
+    let data = JSON.parse(await readContacts());
+    console.log("🚀 ~ countContacts ~ data:", data)
+    let sum = 0;
+    for (let i of data) {
+        sum++;
+    }
+    return sum;
+};
 
 console.log(await countContacts());
